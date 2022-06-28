0278cfd058aceae1b7951732067bd1306c8da7d8
harsh yadav <harshy110@gmail.com>
2022-06-28 20:09:23 +0530

    added creds

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ var con = mysql.createConnection({
     host: "teridb.ctgy1xlcobou.ap-south-1.rds.amazonaws.com", // ip address of server running mysql
     user: "admin", // user name to your mysql database
     password: "Sathya12345", // corresponding password
-    database: "teridb", // use the specified database,
     port: "3306"
 });
 
