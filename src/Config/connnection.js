3b368a7c3d26871623da64fef27b90344704443d
harsh yadav <harshy110@gmail.com>
2022-07-22 11:31:30 +0530

    updated code

--- src/Config/connnection.js
+++ src/Config/connnection.js
@@ -1,24 +1,24 @@
 var mysql = require('mysql');
 
 
-// var connection = mysql.createPool({
-//   connectionLimit: 99,
-//   host: "teridb.ctgy1xlcobou.ap-south-1.rds.amazonaws.com", // ip address of server running mysql
-//   user: "admin", // user name to your mysql database
-//   password: "Sathya098765", // corresponding password
-//   port: "3306",
-//   database: "shooolnyt"
-// });
-
-
 var connection = mysql.createPool({
   connectionLimit: 99,
-  host: "teridb-uat.ctgy1xlcobou.ap-south-1.rds.amazonaws.com", // ip address of server running mysql
+  host: "teridb.ctgy1xlcobou.ap-south-1.rds.amazonaws.com", // ip address of server running mysql
   user: "admin", // user name to your mysql database
-  password: "Teri0lympiad2022", // corresponding password
+  password: "Sathya098765", // corresponding password
   port: "3306",
-  database: "greenolympiaddb"
+  database: "shooolnyt"
 });
+
+
+// var connection = mysql.createPool({
+//   connectionLimit: 99,
+//   host: "teridb-uat.ctgy1xlcobou.ap-south-1.rds.amazonaws.com", // ip address of server running mysql
+//   user: "admin", // user name to your mysql database
+//   password: "Teri0lympiad2022", // corresponding password
+//   port: "3306",
+//   database: "greenolympiaddb"
+// });
 
 
 connection.getConnection(function (err) {
